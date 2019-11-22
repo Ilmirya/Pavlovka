30e78e4d775e8a929c986c6542ce2f1f6641e145
yanturin.ilmir <yanturin.ilmir@gmail.com>
2019-11-22 15:46:20 +0500

    вкл/выкл уведомлений: через switches можно включить/выключить уведомления

--- app/src/main/java/com/example/pavlovka/CustomizableOptionsActivity.java
+++ app/src/main/java/com/example/pavlovka/CustomizableOptionsActivity.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class CustomizableOptionsActivity extends AppCompatActivity {
     private EditText etWLSmin2, etWLSmax2, etProc1, etMaxTimeStop, etProc2;
-    private Switch switchAutoQueryByDiscrepancy;
+    private Switch swhAutoQueryByDiscrepancy, swhWlsLessThenWlsminAndStop, swhWlsMoreThenWlsmaxAndStart, swhWlsLessThenWLsmin2AndStart, swhWlsMoreThenWlsmax2AndStart, swhProc1, swhMaxTimeStop, swhProc2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +24,26 @@ public class CustomizableOptionsActivity extends AppCompatActivity {
         etProc1 = findViewById(R.id.etProc1);
         etMaxTimeStop = findViewById(R.id.etMaxTimeStop);
         etProc2 = findViewById(R.id.etProc2);
-        switchAutoQueryByDiscrepancy = findViewById(R.id.swhAutoQueryByDiscrepancy);
+        swhAutoQueryByDiscrepancy = findViewById(R.id.swhAutoQueryByDiscrepancy);
+
+        swhWlsLessThenWlsminAndStop = findViewById(R.id.swhWlsLessThenWLsmin);
+        swhWlsMoreThenWlsmaxAndStart = findViewById(R.id.swhWlsMoreThenWlsmax);
+        swhWlsLessThenWLsmin2AndStart = findViewById(R.id.swhWLSmin2);
+        swhWlsMoreThenWlsmax2AndStart = findViewById(R.id.swhtvWLSmax2);
+        swhProc1 = findViewById(R.id.swhtvProc1);
+        swhMaxTimeStop = findViewById(R.id.swhtvMaxTimeStop);
+        swhProc2 = findViewById(R.id.swhtvProc2);
+
         try {
-            switchAutoQueryByDiscrepancy.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("AutoQueryByDiscrepancy", "false",this)));
+            swhWlsLessThenWlsminAndStop.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("isWlsLessThenWlsminAndStop", "false",this)));
+            swhWlsMoreThenWlsmaxAndStart.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("isWlsMoreThenWlsmaxAndStart", "false",this)));
+            swhWlsLessThenWLsmin2AndStart.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("isWlsLessThenWLsmin2AndStart", "false",this)));
+            swhWlsMoreThenWlsmax2AndStart.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("isWlsMoreThenWlsmax2AndStart", "false",this)));
+            swhProc1.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("isProc1", "false",this)));
+            swhMaxTimeStop.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("isMaxTimeStop", "false",this)));
+            swhProc2.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("isProc2", "false",this)));
+
+            swhAutoQueryByDiscrepancy.setChecked(Boolean.parseBoolean(Util.getPropertyOrSetDefaultValue("AutoQueryByDiscrepancy", "false",this)));
             etWLSmin2.setText(Util.getPropertyOrSetDefaultValue("WLSmin2", "9",this));
             etWLSmax2.setText(Util.getPropertyOrSetDefaultValue("WLSmax2", "13.75",this));
             etProc1.setText(Util.getPropertyOrSetDefaultValue("Proc1", "10",this));
@@ -43,7 +60,15 @@ public class CustomizableOptionsActivity extends AppCompatActivity {
     }
     public void onClickSaveOptions(View view){
         try {
-            Util.setPropertyConfig("AutoQueryByDiscrepancy", Boolean.toString(switchAutoQueryByDiscrepancy.isChecked()), this);
+            Util.setPropertyConfig("isWlsLessThenWlsminAndStop", Boolean.toString(swhWlsLessThenWlsminAndStop.isChecked()), this);
+            Util.setPropertyConfig("isWlsMoreThenWlsmaxAndStart", Boolean.toString(swhWlsMoreThenWlsmaxAndStart.isChecked()), this);
+            Util.setPropertyConfig("isWlsLessThenWLsmin2AndStart", Boolean.toString(swhWlsLessThenWLsmin2AndStart.isChecked()), this);
+            Util.setPropertyConfig("isWlsMoreThenWlsmax2AndStart", Boolean.toString(swhWlsMoreThenWlsmax2AndStart.isChecked()), this);
+            Util.setPropertyConfig("isProc1", Boolean.toString(swhProc1.isChecked()), this);
+            Util.setPropertyConfig("isMaxTimeStop", Boolean.toString(swhMaxTimeStop.isChecked()), this);
+            Util.setPropertyConfig("isProc2", Boolean.toString(swhProc2.isChecked()), this);
+
+            Util.setPropertyConfig("AutoQueryByDiscrepancy", Boolean.toString(swhAutoQueryByDiscrepancy.isChecked()), this);
             Util.setPropertyConfig("WLSmin2", etWLSmin2.getText().toString(),this);
             Util.setPropertyConfig("WLSmax2", etWLSmax2.getText().toString(),this);
             Util.setPropertyConfig("Proc1", etProc1.getText().toString(),this);
